1ef5c827138e880676265a0781bf6422a1ce60fe
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-04 13:10:53 +0700

    add: category not found error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,13 +3,18 @@ const errorHandler = (err, req, res, next) => {
     switch (err.name) {
       case "AddedDatabase":
         res.status(400).json({
-          message: "Sorry. Database already exist"
+          message: "Sorry. Database already exist."
+        })
+        break;
+      case "CategoryNotFound":
+        res.status(404).json({
+          message: "Sorry. Category doesn't exist."
         })
         break;
       default:
         console.log(err)
         res.status(500).json({
-          message: "Internal server error"
+          message: "Internal server error."
         })
         break;
     }
