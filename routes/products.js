e9ccf5c82d751d9554fea0702087f6dfe53539a9
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-04 14:18:03 +0700

    add: psot and put endpoint

--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,8 @@ const ProductController = require('../controllers/ProductController')
 
 router.get('/', ProductController.getProducts)
 router.get('/categories/:id', ProductController.getProductByCategory)
+router.get('/xml', ProductController.getProductsXML)
+router.post('/', ProductController.addProduct)
+router.put('/:id', ProductController.editProductById)
 
 module.exports = router
